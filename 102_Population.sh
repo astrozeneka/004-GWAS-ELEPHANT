6ea3a9fa34b8a4a2609e17fd91c673853165074c
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2023-06-23 15:45:33 +0700

    Add 103 and 102

---
+++ 102_Population.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -p memory
+#SBATCH -N 1 -c 120
+#SBATCH -t 24:00:00
+#SBATCH -J population
+#SBATCH -A proj5034
+
+module load foss/2021b
+export PATH=$PATH:/tarafs/data/project/proj5034-AGBKU/local/bin/
+
+
+source ./variables.txt
+group="tusk"
+export STACK_RESULT_DIRECTORY="/tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/result_stacks_${tusk}_2"
+export POPULATION_RESULT_DIRECTORY="/tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/result_population_${group}_2"
+
+populations -P "${STACK_RESULT_DIRECTORY}" \
+  -O "${POPULATION_RESULT_DIRECTORY}" \
+  -M "popmap/EMA_popmap_tusk.txt" \
+  --min-samples-per-pop 0.70 \
+  --min-maf 0.05 \
+  --max-obs-het 0.7 \
+  --fstats \
+  --fst-correction \
+  --genepop \
+  --vcf \
+  --plink \
+  --structure \
+  --gtf \
+  --fasta-loci \
+  --fasta-samples \
+  -t 120
