b3de6d21119ec5cdb4fd840ba8bad0c6d5b87257
Ryan R <rasoarahonarivoniaina.h@ku.th>
2023-08-03 15:25:51 +0700

    Update 404

--- 404_Gemma_Tusk.sh
+++ 404_Gemma_Tusk.sh
@@ -11,14 +11,18 @@ conda activate gemma
 
 source variables.txt
 export PLINK_RESULT_DIRECTORY="/tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/result_plink_tusk_2"
-export GEMMA_OUTPUT_DIRECTORY="/tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/gemma_tusk/tusk"
+export GEMMA_OUTPUT_DIRECTORY="/tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/gemma_tusk"
 export PHENO_FILE="/tarafs/data/home/hrasoara/proj5034-AGBKU/map_bwa_ema_Ryan/gemma_tusk/pheno.txt"
 
 #1
 gemma -bfile "${PLINK_RESULT_DIRECTORY}/snp-tusk" \
 -gk -p "${PHENO_FILE}" \
 -o "gemma_tusk"
+mv output/* "${GEMMA_OUTPUT_DIRECTORY}/"
+rm -R output
 
+#rm -R "${GEMMA_OUTPUT_DIRECTORY}"
+#mv -R "gemma_tusk" "${GEMMA_OUTPUT_DIRECTORY}"
 
 #2
 #gemma -bfile /tarafs/data/project/proj5034-AGBKU/map_bwa_ema_Ryan/plink_ema_hair/snp.hair -k gemma_kinship.cXX.txt -lmm 1 -o gemma_lmm1
